7e57535a5b8a8d87ee2410eb67ef309691e46b05
Andrii Slabkyi <aslabkyi@gmail.com>
2023-03-22 17:05:30 +0200

    proptypes change

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -49,21 +49,3 @@ export class Feedback extends Component {
     );
   }
 }
-
-Feedback.propTypes = {
-  good: PropTypes.number.isRequired,
-  neutral: PropTypes.number.isRequired,
-  bad: PropTypes.number.isRequired,
-  onLeaveFeedback: PropTypes.func.isRequired,
-  total: PropTypes.number.isRequired,
-  positivePercentage: PropTypes.string.isRequired,
-};
-
-Feedback.defaultProps = {
-  good: 0,
-  neutral: 0,
-  bad: 0,
-  onLeaveFeedback: () => {},
-  total: 0,
-  positivePercentage: '0%',
-};
